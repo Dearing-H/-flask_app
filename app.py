006e9cc12d2277bb64d1166729133f504db64837
Harrison <hdearing@student.saintaug.nsw.edu.au>
2024-11-14 09:56:21 +1100

    Add Flask app and database setup code

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ from flask import Flask, render_template, request, flash, session
 from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
-from models import user 
-
+from models import User
 app = Flask(__name__)
 app.secret_key = "supersecretkey"
 
@@ -14,14 +13,39 @@ db_session = session()
 
 #define user details
 username = "Boss"
-password =
+password = "Bstream1"
+
+#hash` the password
+hashed_password = generate_password_hash(password)
+
+#create a new user instance
+new_user = User(username=username, password=hashed_password)
+
+#add the new user to the database
+db_session.add(new_user)
+db_session.commit()
+print("User added to the database")
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/login')
+@app.route('/login', methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        #query the database for the user
+        user = db_session.query(User).filter_by(username=username).first()
+
+        if user and check_password_hash(user.password, password):
+            flash("You have successfully logged in", "success")
+            session["user"] = username
+            return render_template('index.html')
+        else:
+            flash("Invalid username or password", "danger")
+            return render_template('login.html')
     return render_template('login.html')
 
 if __name__ == '__main__':
